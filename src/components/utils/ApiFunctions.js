daa9a0873da011b7c05034b87fe8999c7e9e6630
Infantkaviya <infantkaviya@gmail.com>
2025-08-27 22:40:01 +0530

    Update ApiFunctions.js

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://hotelbookingapplication-64qw.onrender.com",
 })
 
 export const getHeader = () => {
