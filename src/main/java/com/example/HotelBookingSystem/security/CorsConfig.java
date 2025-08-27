3dcaad58592f6b766ed9abada2915b3ae2862a49
Infantkaviya <infantkaviya@gmail.com>
2025-08-27 23:07:52 +0530

    Update CorsConfig.java

--- src/main/java/com/example/HotelBookingSystem/security/CorsConfig.java
+++ src/main/java/com/example/HotelBookingSystem/security/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://hotelbookingapplication-64qw.onrender.com");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
