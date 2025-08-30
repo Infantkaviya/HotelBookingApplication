0eda7a2fb5eb8950cfff43c0a07e2f339c1596a2
Infantkaviya <infantkaviya@gmail.com>
2025-08-30 14:15:13 +0530

    Update RoomController.java

--- src/main/java/com/example/HotelBookingSystem/controller/RoomController.java
+++ src/main/java/com/example/HotelBookingSystem/controller/RoomController.java
@@ -36,7 +36,7 @@ public class RoomController {
     private final BookingService bookingService;
 
     @PostMapping("/add/new-room")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+
     public ResponseEntity<RoomResponse> addNewRoom(
             @RequestParam("photo") MultipartFile photo,
             @RequestParam("roomType") String roomType,
@@ -75,7 +75,7 @@ public class RoomController {
     }
 
     @PutMapping("/update/{roomId}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    
     public ResponseEntity<RoomResponse> updateRoom(@PathVariable Long roomId,
                                                    @RequestParam(required = false)  String roomType,
                                                    @RequestParam(required = false) BigDecimal roomPrice,
